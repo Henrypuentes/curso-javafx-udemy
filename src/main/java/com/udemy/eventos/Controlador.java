679b260c71183d06c46c62944c757840499962b1
Rene Enriquez <enriquezrene.ap@gmail.com>
2016-06-15 09:07:06 -0500

    Carga inicial del curso de JavaFX en udemy

---
+++ src/main/java/com/udemy/eventos/Controlador.java
@@ -0,0 +1,13 @@
+package com.udemy.eventos;
+
+/**
+ * Created by moe on 6/13/16.
+ */
+
+
+  public class Controlador {
+
+      public void accion(){
+          System.out.println("El botón ha sido presionado");
+      }
+  }
