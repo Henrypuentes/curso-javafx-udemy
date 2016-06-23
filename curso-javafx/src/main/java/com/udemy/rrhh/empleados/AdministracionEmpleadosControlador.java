ccfa641cdcd97f3d8e9c980df5226143562d7196
Rene Enriquez <renriquez@ioet.com>
2016-06-23 17:40:33 -0500

    layout

---
+++ curso-javafx/src/main/java/com/udemy/rrhh/empleados/AdministracionEmpleadosControlador.java
@@ -0,0 +1,10 @@
+package com.udemy.rrhh.empleados;
+
+
+import com.udemy.rrhh.layout.ControladorConNavegabilidad;
+
+/**
+ * Created by rene on 22/06/16.
+ */
+public class AdministracionEmpleadosControlador extends ControladorConNavegabilidad {
+}
