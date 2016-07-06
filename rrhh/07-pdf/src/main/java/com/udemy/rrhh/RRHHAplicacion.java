6cf2b8f5a3497345b53cc27bbcb7b465484871f3
Rene Enriquez <enriquezrene.ap@gmail.com>
2016-07-06 00:25:08 -0500

    pdf support con pdf js

---
+++ rrhh/07-pdf/src/main/java/com/udemy/rrhh/RRHHAplicacion.java
@@ -0,0 +1,38 @@
+package com.udemy.rrhh;
+
+
+import com.udemy.rrhh.barramenu.BarraMenuControlador;
+import com.udemy.rrhh.departamentos.AdministracionDepartamentosControlador;
+import com.udemy.rrhh.empleados.AdministracionEmpleadosControlador;
+import com.udemy.rrhh.layout.LayoutPane;
+import com.udemy.rrhh.reporte.EmpleadosPorDepartamentoControlador;
+import com.udemy.rrhh.webview.WebviewControlador;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by rene on 21/06/16.
+ */
+public class RRHHAplicacion extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        LayoutPane layoutPane = new LayoutPane();
+
+        layoutPane.cargarPantalla("departamentos", AdministracionDepartamentosControlador.class.getResource("AdministracionDepartamentos.fxml"));
+        layoutPane.cargarPantalla("empleados", AdministracionEmpleadosControlador.class.getResource("AdministracionEmpleados.fxml"));
+        layoutPane.cargarPantalla("chart", EmpleadosPorDepartamentoControlador.class.getResource("EmpleadosPorDepartamento.fxml"));
+        layoutPane.cargarPantalla("menu", BarraMenuControlador.class.getResource("BarraMenu.fxml"));
+        layoutPane.cargarPantalla("webview", WebviewControlador.class.getResource("WebView.fxml"));
+
+        layoutPane.cargarBarraDeMenuEnLaPartePosterior();
+
+        Scene escena = new Scene(layoutPane, 900, 700);
+        escena.getStylesheets().addAll(getClass().getResource("estilos.css").toExternalForm());
+        primaryStage.setScene(escena);
+        primaryStage.show();
+    }
+
+}
