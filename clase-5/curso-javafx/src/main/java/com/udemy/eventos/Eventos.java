89ec0a350ca3a88a1613e5a024e8abea665bcc56
Rene Enriquez <enriquezrene.ap@gmail.com>
2016-07-10 13:33:56 -0500

    clase 5

---
+++ clase-5/curso-javafx/src/main/java/com/udemy/eventos/Eventos.java
@@ -0,0 +1,21 @@
+package com.udemy.eventos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Eventos extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent contenedor = new FXMLLoader().load(getClass().getResource("Eventos.fxml"));
+
+        Scene escena = new Scene(contenedor, 200, 100);
+        primaryStage.setScene(escena);
+        primaryStage.show();
+    }
+
+}
